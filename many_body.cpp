21311ddaa6c9b710b1e64969649ecbd300f99e7c
manu-web <manu.maheshwari78@gmail.com>
2019-03-04 19:47:58 +0530

    Updated many_body.cpp file

--- many_body.cpp
+++ many_body.cpp
@@ -7,3 +7,33 @@
 #define length 100
 #define breadth 200
 #define height 400
+#define time_step 0.01
+
+void find_force(ll **F)
+{
+     
+}
+
+void velocity_update(ll **F,ll **V)
+{
+
+}
+
+void collision_and_position_update(ll **R,ll **V)
+{
+
+}
+
+int main()
+{
+	int i;
+	
+	return 0;
+}
+
+
+
+
+
+
+
