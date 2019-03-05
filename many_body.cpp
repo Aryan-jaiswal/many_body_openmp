2255a08af6efbdb1e3de026955e91feffdf5a283
Aryan-jaiswal <ajaiswal60@yahoo.com>
2019-03-06 00:51:56 +0530

    Read and generated trajecrories at different timestep

--- many_body.cpp
+++ many_body.cpp
@@ -12,18 +12,21 @@
 #define breadth 200
 #define height 400
 #define delta_t 0.01
-#define timestep 7//720000
+#define radius 0.5
+#define timestep 100//720000
+#define R_array false
+#define F_array true
 
 using namespace std;
 
-void find_force(double F[][3],double R[][3],ll dim)    
+void find_force(double **F,double **R)    
 {
      ll i,j;
      double x,y,z,dis;
-     for(i=0;i<dim;i++)
+     for(i=0;i<number_bodies;i++)
      {
      	x=y=z=0.0;
-     	for(j=0;j<dim;j++)
+     	for(j=0;j<number_bodies; j++)
         {
         	dis=sqrt((R[j][0]-R[i][0])*(R[j][0]-R[i][0]) + (R[j][1]-R[i][1])*(R[j][1]-R[i][1]) + (R[j][2]-R[i][2])*(R[j][2]-R[i][2]));
         	if(dis!=0)
@@ -39,48 +42,126 @@ void find_force(double F[][3],double R[][3],ll dim)
      }
 }
 
-void velocity_update(double F[][3],double V[][3],ll dim)
+void velocity_update(double **F,double **V)
 {
-    ll i;
-    for(i=0;i<dim;i++)
+    int i;
+    for(i=0;i<number_bodies;i++)
     {
-    	V[i][0]=V[i][0]+(F[i][0]*delta_t)/(2.0*mass);
-    	V[i][1]=V[i][1]+(F[i][1]*delta_t)/(2.0*mass);
-    	V[i][2]=V[i][2]+(F[i][2]*delta_t)/(2.0*mass);
+    	V[i][0] = V[i][0]+(F[i][0]*delta_t)/(2.0*mass);
+    	V[i][1] = V[i][1]+(F[i][1]*delta_t)/(2.0*mass);
+    	V[i][2] = V[i][2]+(F[i][2]*delta_t)/(2.0*mass);
     }
 }
 
-void collision_and_position_update(ll **R,ll **V)
+void collision_and_position_update(double **R,double **V)
 {
     
+	////////////////position update if no collision//////////
+    int i;
+    for(i=0;i<number_bodies;i++)
+    {
+    	R[i][0] += V[i][0]*delta_t;
+    	R[i][1] += V[i][1]*delta_t;
+    	R[i][2] += V[i][2]*delta_t;
+    }
+    ///////////////////////////////////
 }
 
-void generate_bin_file(double R[][3],ll num,ll dim)
+void generate_bin_file(double **R,int num)
 {
     fstream file;
     string s="trajectory"+to_string(num)+".bin";
 	file.open(s,ios::out|ios::binary);
 
-	for(ll i=0;i<dim;i++)
+	for(ll i=0;i<number_bodies;i++)
 	{
-		file.write((char*)&R[i][0],sizeof(R[i][0]));
-		file.write((char*)&R[i][1],sizeof(R[i][1]));
-		file.write((char*)&R[i][2],sizeof(R[i][2]));
+		// file.write((char*)&R[i][0],sizeof(R[i][0]));
+		// file.write((char*)&R[i][1],sizeof(R[i][1]));
+		// file.write((char*)&R[i][2],sizeof(R[i][2]));
+        file<<R[i][0]<<" ";
+        file<<R[i][1]<<" ";
+        file<<R[i][2]<<" ";
+        file <<"\n";
 	}
 	file.close();
 }
 
+void read_txt_file(double **R){
+
+    fstream init("trajectory.txt", std::ios_base::in);
+    if(init.is_open())
+    {
+        int c = 0;
+        while (c<1000)
+        {   
+            init >> R[c][0] >> R[c][1] >> R[c][2];
+            c++;   
+        }
+
+        return;
+    }
+    else
+    {   cout<<"Unable to open file!"<<endl;
+        exit (EXIT_FAILURE);
+    }
+    init.close();
+}
+void print_2d(bool f,bool r,double **R,double **F)
+{
+    if(f){
+        for (int i = 0; i < 1000; ++i)
+        {
+            cout<< R[i][0]<< " " <<R[i][1]<< " " <<R[i][2]<< " "<<endl; 
+        }
+    }
+    if(r)
+    {
+        for (int i = 0; i < 1000; ++i)
+        {
+            cout<< F[i][0]<< " " <<F[i][1]<< " " <<F[i][2]<< " "<<endl; 
+        }
+    }
+    return;
+}
 int main()
 {
 	int i;
+    ////////////////initialise position array////////////////
+    double **R;
+    R = (double**)malloc(sizeof(double*)*1000);
+    for(int i = 0; i <1000; i++)
+        R[i] = (double*)malloc(sizeof(double)*3);
+    read_txt_file(R);
+    ///////////////////////////////////////////////////////
+
+    ///////////////Initialise Force matrix////////////////
+    double **F;
+    F = (double**)malloc(sizeof(double*)*1000);
+    for(int i = 0; i <1000; i++)
+        F[i] = (double*)malloc(sizeof(double)*3);
+    ///////////////////////////////////////////////////////
+
+    ///////////////Initialise Velocity matrix////////////////
+    double **V;
+    V = (double**)calloc(1000, sizeof(double*));
+    for(int i = 0; i <1000; i++)
+        V[i] = (double*)calloc(3, sizeof(double));
+    ///////////////////////////////////////////////////////
+
+
 	for(int i = 0 ;i <timestep ; i++){
-		find_force();
-		velocity_update();
-		collision_and_position_update();
-		velocity_update();
+		find_force(F,R);
+		velocity_update(F,V);
+		collision_and_position_update(R,V);
+		velocity_update(F,V);
 		if(i%100 == 0)
-			generate_bin_file();
+        {
+            generate_bin_file(R,i);
+        }
 	}
+    if(R_array || F_array){ //set R_array to view initial matrix
+        print_2d(R_array,F_array,R,F);
+    }
 	return 0;
 }
 
