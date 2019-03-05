ee6b01dca4940ce9d22d15696978565d5f32a67d
Aryan-jaiswal <ajaiswal60@yahoo.com>
2019-03-06 03:43:19 +0530

    Added Elastic Collision between bodies

--- many_body.cpp
+++ many_body.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string>
+#include <omp.h>
 
 #define ll long long int
 #define number_bodies 1000
@@ -13,15 +14,27 @@
 #define height 400
 #define delta_t 0.01
 #define radius 0.5
-#define timestep 100//720000
-#define R_array true
-#define F_array false
+#define timestep 101//720000
+#define print_pose false
+#define print_force false
+#define print_distance false
+#define DOUBLE_MAX 999999.99
+#define COLLISION_CHECK
 
 using namespace std;
 
 float euclidean(double x1, double y1, double z1, double x2, double y2, double z2){
 
 	return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
+}
+
+void print_matrix(double **X)
+{
+        for (int i = 0; i < 1000; ++i)
+        {
+            cout<< X[i][0]<< " " <<X[i][1]<< " " <<X[i][2]<< " "<<endl; 
+        }
+    return;
 }
 
 void find_force(double **F,double **R)    
@@ -58,9 +71,73 @@ void velocity_update(double **F,double **V)
     }
 }
 
-void collision_and_position_update(double **R,double **V)
+void initialise_distance(double **D)
 {
-    //////Inter-Collision Check////////////
+	int i = 0 , j = 0;
+	for( i = 0 ; i < 1000; i++)
+		for ( j = 0 ; j < 1000 ; j++)
+			D[i][j] = DOUBLE_MAX;
+	return;
+}
+
+void compute_distance(double **D, double **R , double **V)
+{
+	int i = 0 , j = 0;
+	float tolerance = 2*radius;
+
+	#ifndef OPENMP
+	{
+		for( i = 0 ; i < 1000; i++)
+			for ( j = i+1 ; j < 1000 ; j++)
+			{
+				D[i][j] = D[j][i] = euclidean(R[j][0],R[j][1],R[j][2],R[i][0],R[i][1],R[i][2]);
+
+				////////Updating Position if distance is less than tolerance////
+				if(D[i][j] <= tolerance)
+				{
+					V[i][0] = V[j][0];
+					V[i][1] = V[j][1];
+					V[i][2] = V[j][2];
+					R[i][0] = R[j][0];
+					R[i][1] = R[j][1];
+					R[i][2] = R[j][2];
+				}
+				///////////////////////////////////////////////////
+
+			}
+	}
+	#endif
+
+	#ifdef OPENMP
+	{
+		for( i = 0 ; i < 1000; i++)
+		{
+			for ( j = 0 ; j < 1000 ; j++)
+			{
+				if( i!=j) //check if it will work upon parallezing 
+					D[i][j] = euclidean(R[j][0],R[j][1],R[j][2],R[i][0],R[i][1],R[i][2]);
+
+				////////Updating Position and Velocity if distance is less than tolerance////
+				if(D[i][j] <= tolerance)
+				{
+					V[i][0] = V[j][0];
+					V[i][1] = V[j][1];
+					V[i][2] = V[j][2];
+					R[i][0] = R[j][0];
+					R[i][1] = R[j][1];
+					R[i][2] = R[j][2];
+				}
+				///////////////////////////////////////////////////////////////
+			}
+		}
+	}
+	#endif
+
+	return;
+}
+
+void collision_and_position_update(double **R,double **V, double **D)
+{
 
 	//////position update//////////
     int i;
@@ -87,69 +164,65 @@ void collision_and_position_update(double **R,double **V)
     		V[i][2] = -V[i][2];
     	}
     	/////////////////////////////////
-
     }
+    /////////Collision Check between bodies//////////
+    	#ifdef COLLISION_CHECK
+    	{
+		    compute_distance(D,R,V);
+
+			if(print_distance) print_matrix(D);
+    	}
+    	#endif
+    /////////////////////////////////////////////////
 }
 
 void generate_bin_file(double **R,int num)
 {
     fstream file;
-    string s="trajectory"+to_string(num)+".bin";
+    string s = "trajectory"+to_string(num)+".bin";
 	file.open(s,ios::out|ios::binary);
-
-	for(ll i=0;i<number_bodies;i++)
+	int i;
+	for(i=0 ; i < number_bodies ; i++)
 	{
 		// file.write((char*)&R[i][0],sizeof(R[i][0]));
 		// file.write((char*)&R[i][1],sizeof(R[i][1]));
 		// file.write((char*)&R[i][2],sizeof(R[i][2]));
-        file<<R[i][0]<<" ";
-        file<<R[i][1]<<" ";
-        file<<R[i][2]<<" ";
+        file << R[i][0] << " ";
+        file << R[i][1] << " ";
+        file << R[i][2] <<  " ";
         file <<"\n";
 	}
 	file.close();
 }
 
+//Reads the initial coordinates of 1000 balls//
 void read_txt_file(double **R){
 
     fstream init("trajectory.txt", std::ios_base::in);
+
     if(init.is_open())
     {
-        int c = 0;
-        while (c<1000)
+        int idx = 0;
+        while (idx < 1000)
         {   
-            init >> R[c][0] >> R[c][1] >> R[c][2];
-            c++;   
+            init >> R[idx][0] >> R[idx][1] >> R[idx][2];
+            idx++;   
         }
-
+        init.close();
         return;
     }
     else
-    {   cout<<"Unable to open file!"<<endl;
+    {   cout << "Unable to open file!" << endl;
         exit (EXIT_FAILURE);
     }
-    init.close();
 }
-void print_2d(bool f,bool r,double **R,double **F)
-{
-    if(f){
-        for (int i = 0; i < 1000; ++i)
-        {
-            cout<< R[i][0]<< " " <<R[i][1]<< " " <<R[i][2]<< " "<<endl; 
-        }
-    }
-    if(r)
-    {
-        for (int i = 0; i < 1000; ++i)
-        {
-            cout<< F[i][0]<< " " <<F[i][1]<< " " <<F[i][2]<< " "<<endl; 
-        }
-    }
-    return;
-}
+
 int main()
 {
 	int i;
+
+	///Divide each initialissation into sections in openmp///////
+
     ////////////////initialise Position matrix////////////////
     double **R;
     R = (double**)malloc(sizeof(double*)*1000);
@@ -172,6 +245,14 @@ int main()
         V[i] = (double*)calloc(3, sizeof(double));
     ///////////////////////////////////////////////////////
 
+    ////////////////Initialise Distance matrix//////////////
+    double **D;
+    D = (double**)malloc(sizeof(double*)*1000);
+    for(i = 0; i <1000; i++)
+        D[i] = (double*)malloc(sizeof(double)*1000);
+    initialise_distance(D);
+    ///////////////////////////////////////////////////////
+
 
 	for( i = 0 ;i <timestep ; i++){
 		
@@ -179,7 +260,7 @@ int main()
 		
 		velocity_update(F,V);
 		
-		collision_and_position_update(R,V);
+		collision_and_position_update(R,V,D);
 		
 		velocity_update(F,V);
 		
@@ -189,9 +270,11 @@ int main()
         }
 	}
     
-    if(R_array || F_array){ //set R_array to view initial matrix
-        print_2d(R_array,F_array,R,F);
-    }
+    if(print_pose) //set print_pose to view initial matrix
+        print_matrix(R);
+    else if (print_force)
+    	print_matrix(F);
+
 	return 0;
 }
 
