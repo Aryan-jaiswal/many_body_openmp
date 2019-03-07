d76db535bfd01550a02d392482566426ff61df50
Aryan-jaiswal <ajaiswal60@yahoo.com>
2019-03-07 22:06:12 +0530

    Added mayavi

--- visualiser.py
+++ visualiser.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python2
 
 from numpy import *
-from numpy import linalg
 import numpy as np
-import pygame
 import time
 
+pygame = 0
+mayavi = 1
 def get_trajectory(filename): 
     data =[]
     with open(filename) as input_file:
@@ -21,38 +21,50 @@ def get_trajectory(filename):
 num = 0
 filename = "trajectory" +str(num) + ".bin"
 data  = get_trajectory(filename)
+if pygame:
+    # Pygame initialzation
+    import pygame
+    pygame.init()
+    xResolution = 640
+    yResolution = 480
+    surface = pygame.display.set_mode((xResolution, yResolution))
+    xCenter = int(xResolution / 2)
+    yCenter = int(yResolution / 2)
+    font = pygame.font.Font(None, 20)
 
-# Pygame initialzation
-pygame.init()
-xResolution = 640
-yResolution = 480
-surface = pygame.display.set_mode((xResolution, yResolution))
-xCenter = int(xResolution / 2)
-yCenter = int(yResolution / 2)
-font = pygame.font.Font(None, 20)
+    # Loop timesteps forever
+    timestep = 0
+    while True:
 
-# Loop timesteps forever
-timestep = 0
-while True:
+        filename = "trajectory" +str(timestep) + ".bin"
+        # Increment timestep
+        timestep = timestep + 100
 
-    filename = "trajectory" +str(timestep) + ".bin"
-    # Increment timestep
-    timestep = timestep + 100
+        # Pygame update screen
+        pygame.display.update()
 
-    # Pygame update screen
-    pygame.display.update()
+        surface.fill((0,0,0))
+        data = get_trajectory(filename)
 
-    surface.fill((0,0,0))
-    data = get_trajectory(filename)
+        for i in range(0, len(data)):
 
-    for i in range(0, len(data)):
+            # Pygame draw body
+            pygame.draw.circle(surface, (random.triangular(0,125,255),random.triangular(0,125,255),random.triangular(0,125,255)), (int(data[i][0]*640/200), int(data[i][1]*480/100)), 3 ,0)
 
-        # Pygame draw body
-        pygame.draw.circle(surface, (random.triangular(0,125,255),random.triangular(0,125,255),random.triangular(0,125,255)), (int(data[i][0]*640/200), int(data[i][1]*480/100)), 3 ,0)
+            # Pygame write elapsed time
+            text = font.render("Time: " + str(int(timestep * 0.1))+"s", True, (255,255,255))
+            surface.blit(text,(10,10))
+        time.sleep(0.1)
+if mayavi:
+    from mayavi import mlab
+    from mayavi.mlab import *
+    timestep = 0
+    while True:    
+        
+        filename = "trajectory" +str(timestep) + ".bin"
+        # Increment timestep
+        timestep = timestep + 100
 
-        # Pygame write elapsed time
-        text = font.render("Time: " + str(int(timestep * 0.1))+"s", True, (255,255,255))
-        surface.blit(text,(10,10))
-    time.sleep(0.1)
-
-
+        data = get_trajectory(filename)
+        points3d(zip(*data)[0], zip(*data)[1], zip(*data)[2], colormap="copper", scale_factor=.25)
+        
